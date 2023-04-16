46f71ba2dc9fad453501dd7e5ff77cfe53810d18
Shivangi <shivangit1111@gmail.com>
2023-04-16 19:30:19 +0530

    feat: add base URL for the web service

--- app/src/main/java/com/example/api/network/MarsApiService.kt
+++ app/src/main/java/com/example/api/network/MarsApiService.kt
@@ -1,4 +1,11 @@
 package com.example.api.network
 
+import retrofit2.Retrofit
+
 class MarsApiService {
+
+    private val BASE_URL =
+        "https://android-kotlin-fun-mars-server.appspot.com"
+
+    private val retrofit = Retrofit.Builder()
 }
