8e3c1d3e6a4de225cb50e4c4ccdfcf3cb48c1362
Shivangi <shivangit1111@gmail.com>
2023-04-17 16:44:09 +0530

    feat: view bind

--- app/src/main/java/com/example/api/MyRetrofit.kt
+++ app/src/main/java/com/example/api/MyRetrofit.kt
@@ -2,10 +2,13 @@ package com.example.api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.api.databinding.ActivityMyRetrofitBinding
 
 class MyRetrofit : AppCompatActivity() {
+    private lateinit var binding: ActivityMyRetrofitBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_retrofit)
+        binding = ActivityMyRetrofitBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
