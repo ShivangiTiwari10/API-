06b85783d72949eddf1549bf2aaa12341289416d
Shivangi <shivangit1111@gmail.com>
2023-04-17 16:41:34 +0530

    feat: start activity

--- app/src/main/java/com/example/api/MainActivity.kt
+++ app/src/main/java/com/example/api/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.api
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -11,6 +12,7 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +26,10 @@ class MainActivity : AppCompatActivity() {
             val result = quotesApi.getQuotes()
             Log.d("ayush: ", result.body().toString())
         }
-
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MyRetrofit::class.java)
+            startActivity(intent)
+        }
     }
+
 }
