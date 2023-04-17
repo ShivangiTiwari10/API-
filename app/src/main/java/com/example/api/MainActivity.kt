9238ae61732b7b3869733b8d3dc2e33aeb3ef649
Shivangi <shivangit1111@gmail.com>
2023-04-17 13:49:04 +0530

    core: GET request from interface

--- app/src/main/java/com/example/api/MainActivity.kt
+++ app/src/main/java/com/example/api/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.api.model.QuotesApi
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val quotesApi = RetrofitHelper.getInstance().create(QuotesApi::class.java)
+        // launching a new coroutine
+        GlobalScope.launch {
+            val result = quotesApi.getQuotes()
+            Log.d("ayush: ", result.body().toString())
+        }
+
     }
 }
--- app/src/main/java/com/example/api/model/QuotesApi.kt
+++ app/src/main/java/com/example/api/model/QuotesApi.kt
@@ -1,4 +1,10 @@
 package com.example.api.model
 
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface QuotesApi {
+
+    @GET("/quotes")
+    suspend fun getQuotes() : Response<QuoteList>
 }
