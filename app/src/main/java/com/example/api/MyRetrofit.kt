34e5c001e8001ca33acff4d20d69a324c4bd1fae
Shivangi <shivangit1111@gmail.com>
2023-04-17 17:16:20 +0530

    feat: create retrofitBuilder

--- app/src/main/java/com/example/api/MyRetrofit.kt
+++ app/src/main/java/com/example/api/MyRetrofit.kt
@@ -2,7 +2,10 @@ package com.example.api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.api.`interface`.MyApiInterface
 import com.example.api.databinding.ActivityMyRetrofitBinding
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MyRetrofit : AppCompatActivity() {
     private lateinit var binding: ActivityMyRetrofitBinding
@@ -10,5 +13,11 @@ class MyRetrofit : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMyRetrofitBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val retrofitBuilder = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com/")
+            .addConverterFactory((GsonConverterFactory.create()))
+            .build()
+            .create(MyApiInterface::class.java)
     }
 }
